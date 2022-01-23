73ea9e35df1ae18827a4bb0c8c58f494f3a2430f
Kenneth Barbour <kenbarbour@gmail.com>
2022-01-23 18:34:20 -0500

    cli argument handling

--- receive_pass.sh
+++ receive_pass.sh
@@ -5,24 +5,73 @@
 ##
 
 set -uo pipefail
+prog="$0"
+me=$(basename "${prog}")
 rootdir=$(git rev-parse --show-toplevel)
 source "${rootdir}/lib/utils.sh"
 
+function usage() {
+  grep '^##' "$prog" | sed -e 's/^##\s\?//' -e "s/__PROG__/$me/" 1>&2
+}
+
+# defaults
+freq="137M"
+duration=10
+gain=40
+wavfile="pass.wav"
+debug_out="/dev/null"
+
+while (( "$#" ))
+do
+  case $1 in
+
 ## --freq <frequency>           (default: 137M)
-freq="162450000"
+    '--freq')
+      freq=${2}
+      log "frequency set to ${freq}"
+      shift
+      ;;
 
 ## --duration <seconds>         (default: 900)
-duration=10
+    '--duration')
+      duration=${2}
+      log "duration set to ${duration}"
+      shift
+      ;;
 
 ## --gain <integer>             (default: auto)
-gain=40
+    '--gain')
+      gain=${2}
+      log "gain set to ${gain}"
+      shift
+      ;;
 
-## --outfile-prefix <string>    (default: wxrx-out)
-outfile_prefix="pass"
-wavfile="${outfile_prefix}.wav"
+## --outfile <string>           (default: pass.wav)
+    '--outfile')
+      wavfile=${2:-pass.wav}
+      log "will write to ${wavfile}"
+      shift
+      ;;
 
-## --debug
-debug_out="wxrx.log"
+## --debug                      (default: wxrx.log)
+    '--log')
+      debug_out="2"
+      ;;
+
+## --help, -h
+    '--help' | '-h')
+      usage
+      exit
+      ;;
+
+    *)
+      err "Unknown argument %s" ${1}
+      exit 1
+      ;;
+
+  esac
+  shift
+done
 
 # wxtoimg needs this specific sample rate
 sample_rate="22050"
@@ -31,8 +80,10 @@ sample_rate="22050"
 
 
 log "Listening for signal on ${freq} for ${duration} seconds (gain: ${gain})"
-timeout ${duration} rtl_fm -T -f ${freq} -M fm -g ${gain} -s 48000 -r ${sample_rate} -F 9 -A fast 2>>${debug_out} | sox -r ${sample_rate} -t raw -e s -b 16 -c 1 -V1 - ${wavfile}
-log "Exit status: %d" $?
+timeout ${duration} rtl_fm -T -f ${freq} -M fm -g ${gain} -s 48000 -r ${sample_rate} -F 9 -A fast  | sox -r ${sample_rate} -t raw -e s -b 16 -c 1 -V1 - ${wavfile}
+if [ -z "$?" ]; then
+  log "Exit status: %d, try adding --debug flag" $?
+fi
 log "Finished writing to ${wavfile}"
 
 # Verify that a wavfile was created
