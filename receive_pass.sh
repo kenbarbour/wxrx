e259c06a832796a265b0d55a93b392af95ecee3a
Kenneth Barbour <kenbarbour@gmail.com>
2022-01-30 15:38:30 -0500

    reasonable default filenames based on which satellite

--- receive_pass.sh
+++ receive_pass.sh
@@ -18,8 +18,11 @@ function usage() {
 freq="137M"
 duration=10
 gain=40
-wavfile="pass.wav"
-sample_rate="11025"
+sample_rate=11025
+bandwidth=40000
+now=$(date +%s)
+# bandwidth = 2 * (17kHz deviation + 2.4kHz tone) * doppler shift (~1.2kHz)
+# wavfile is determined by options
 
 while (( "$#" ))
 do
@@ -46,26 +49,29 @@ do
       shift
       ;;
 
-## --outfile <string>           (default: pass.wav)
+## --outfile <string>           sets the name of the output file, overrides any -noaa-** options
     '--outfile')
       wavfile=${2:-pass.wav}
       log "will write to ${wavfile}"
       shift
       ;;
 
-## --noaa-15                    alias for --freq 13762000
+## --noaa-15                    alias --freq 137620000 --outfile noaa_15-${now}.wav
     '--noaa-15')
       freq=137620000
+      ${wavfile:="noaa_15-${now}.wav"}
       ;;
 
-## --noaa-18                    alias for --freq 137912500
+## --noaa-18                    alias --freq 137912500 --outfile noaa_18-${now}.wav
     '--noaa-18')
       freq=137912500
+      ${wavfile:="noaa_18-${now}.wav"}
       ;;
 
-## --noaa-19                    alias for --freq 137100000
+## --noaa-19                    alias --freq 137100000 --outfile noaa_19-${now}.wav
     '--noaa-19')
       freq=137100000
+      ${wavfile:="noaa_19-${now}.wav"}
       ;;
 
 # TODO: ## --bias-t, -T                 enable hardware Bias-T power
@@ -91,17 +97,18 @@ do
 done
 
 function demodulate_pass() {
-  timeout ${duration} rtl_fm -T -f ${freq} -M fm -g ${gain} -s 48000 -r ${sample_rate} -F 9 -A fast
+  timeout ${duration} rtl_fm -T -f ${freq} -M fm -g ${gain} -s ${bandwidth} -r ${sample_rate} -F 9 -A fast
 }
 
 function resample_pass() {
-  sox -r ${sample_rate} -t raw -e s -b 16 -c 1 -V1 - ${wavfile}
+  sox -r ${sample_rate} -t raw -e s -b 16 -c 1 -V1 - ${wavfile:=pass.wav}
 }
 
 function monitor_pass() {
   ( [ -z $monitor ] && cat || tee >(play -r ${sample_rate} -t raw -es -b 16 -c 1 -V1 -) )
 }
 
+log "Current timestamp: %s" ${now}
 log "Listening for signal on ${freq} for ${duration} seconds (gain: ${gain})"
 
 demodulate_pass | monitor_pass | resample_pass
